e20422a57b89df92c1c28049e33693c9981a4a2d
Timothy Harrelson <tgharrelson@alaska.edu>
2018-02-28 10:17:09 -0900

    * fixed issue with logic for shifting around region and grid * added logic to speed process up by skipping whole row when found between bands

--- code/hog_generator/hog_generator.py
+++ code/hog_generator/hog_generator.py
@@ -8,34 +8,43 @@ import pickle
 
 # need to clip total_image to be same size as stability mask before using this function
 def create_hog_regions(total_iamge, stability_mask, image_filename, region_size, offset_step_x=1, offset_step_y=1,
-                       orientations=8, pixels_per_cell=(4,4), cells_per_block=(8,8)):
+                       region_threshold=0.8, orientations=8, pixels_per_cell=(4,4), cells_per_block=(8,8)):
     print("Creating ROI HOGs")
 
     roi_file = open(image_filename + "_hog.data", "w")
+    # shifts grid by offset to get slightly different pictures
     for cur_offset_x in range(0, region_size[0], offset_step_x):
         for cur_offset_y in range(0, region_size[1], offset_step_y):
-            region_coords = (cur_offset_x, cur_offset_y, cur_offset_x+region_size[0], cur_offset_y+region_size[1])
-            # for each region in the image, starting at the offset X and Y index
-            while region_coords[2] < total_iamge.shape[0] and region_coords[3] < total_iamge.shape[1]:
-                masked_region = stability_mask[region_coords[0]:region_coords[2], region_coords[1]:region_coords[3]]
-                image_region = total_iamge[region_coords[0]:region_coords[2], region_coords[1]:region_coords[3]]
+            # looks at each tile of the grid with the current offset
+            for cur_region_y in range(0, total_iamge.shape[1]/region_size[1]):
+                for cur_region_x in range(0, total_iamge.shape[0]/region_size[0]):
+                    region_coords = (cur_offset_x + cur_region_x*region_size[0], cur_offset_y + cur_region_y*region_size[1])
+                    """
+                    if region_coords[2] < total_iamge.shape[0] and region_coords[3] < total_iamge.shape[1]:
+                        continue
+                    """
+                    masked_region = stability_mask[region_coords[0]:region_coords[2], region_coords[1]:region_coords[3]]
+                    image_region = total_iamge[region_coords[0]:region_coords[2], region_coords[1]:region_coords[3]]
 
-                # this can be changed to return counts of each unique entry, so we can calculate percents
-                unique = np.unique(masked_region)
-                if unique.shape[0] > 1 or unique.shape[1] > 1:  # if there is >1 type of mask category in region
-                    region_coords += region_size
-                    continue
+                    # this can be changed to return counts of each unique entry, so we can calculate percents
+                    unique, unique_counts = np.unique(masked_region, return_counts=True)
+                    sum_unique = region_size[0]*region_size[1]
+                    unique_counts /= sum_unique     # The percent of the region occupied by each type of mask
+                    if np.where(unique_counts >= region_threshold).size == 0:
+                        break   # we can break this loop, as due to banding, all regions in this row have same issue
+                    """
+                    if unique.shape[0] > 1 or unique.shape[1] > 1:  # if there is >1 type of mask category in region
+                        region_coords += region_size
+                        continue
+                    """
 
-                # create hog for region
-                hog_info = hog(image_region, orientations=orientations, pixels_per_cell=pixels_per_cell,
-                               cells_per_block=cells_per_block, visualise=False, block_norm='L2-Hys')
+                    # create hog for region
+                    hog_info = hog(image_region, orientations=orientations, pixels_per_cell=pixels_per_cell,
+                                   cells_per_block=cells_per_block, visualise=False, block_norm='L2-Hys')
 
-                # save hog info, alongside other relevant info (pixel coords, base image file name)
-                roi_info = (hog_info.dumps(), region_coords)
-                pickle.dump(roi_info, roi_file)
-
-                # increment to next region, no overlap of regions within this loop
-                region_coords += region_size
+                    # save hog info, alongside other relevant info (pixel coords, base image file name)
+                    roi_info = (hog_info.dumps(), region_coords)
+                    pickle.dump(roi_info, roi_file)
 
     roi_file.close()
     print("Done Creating ROI HOGs")

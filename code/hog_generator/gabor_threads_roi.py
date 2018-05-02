b2c27cfcdcecc536e9aa1102ab8d66f1fc64e9a9
Jeremy Swartwood <jeremy.swartwood@gmai.com>
2018-05-01 22:33:53 -0800

    Saves and runs on entire directory

--- code/hog_generator/gabor_threads_roi.py
+++ code/hog_generator/gabor_threads_roi.py
@@ -2,7 +2,10 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt  # Only used for debug imaging
 import sys
+import os
 import time
+from multiprocessing.pool import ThreadPool
+import errno
 
 DEBUG = False
 
@@ -55,25 +58,51 @@ def process(img, filters):
     return combined, kernal_results
 
 
-def run_gabor(color_image, filters, mask):
-    orientations = 16
+def process_threaded(img, filters):
+    kernal_results = np.empty((len(filters), img.shape[0], img.shape[1]), dtype=np.uint8)
+
+    combined = np.zeros_like(img)
+    kern_index = 0
+    def f(kern):
+        return cv2.filter2D(img, cv2.CV_8UC3, kern)
+
+    pool = ThreadPool(processes=8)
+    for fimg in pool.map(f, filters):
+        kernal_results[kern_index, :, :] = fimg  # Put the image into an array we can use it outside
+        kern_index += 1
+        np.maximum(combined, fimg, combined)
+    return combined, kernal_results
+
+
+def run_gabor(color_image, filters, mask, image_filename, orientations=16):
+    directory, filename = os.path.split(image_filename)
+    filename_minus_ext, ext = os.path.splitext(filename)
 
     # Convert to grayscale so we don't get multiple "votes" per pixel per kernal/orientation
     img = cv2.cvtColor(color_image, cv2.COLOR_BGR2GRAY)
 
     # run each of the N Gabor kernals (directions) once across the entire image producing N images (one for each direction/kernal)
-    combined_image, kernal_results = process(img, filters)
+    start_process = time.time()
+    combined_image, kernal_results = process_threaded(img, filters)
+    end_process = time.time()
 
     # From those, at each pixel position, take the maximum response at that position indicating the "direction" of that pixel. 0, 1, 2, .... , N
     # produce an array with these direction values for each pixel
     pixel_directions = np.argmax(kernal_results, axis=0)
     pixel_directions = pixel_directions.astype(np.uint8)
 
-    print(pixel_directions.shape)
-
+    # Create the folder to put the HOG files if none exists. Try except handles race condition, unlike straight makedirs
     output_directory = './HOG_GABOR/'
-    image_filename = 'baboon'
-    # output_file = open(output_directory + image_filename + "_hog.csv", 'w')
+    try:
+        os.makedirs(output_directory)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print("Could not create or find HOG Files directory")
+            raise
+
+    output_file = open(output_directory + filename_minus_ext + "_hog.csv", 'w')
+
+    band_map = np.unique(mask)
 
     # grab ROI from the produced array (say 45x45, or whatever)
     roi_size_y = 31
@@ -85,8 +114,10 @@ def run_gabor(color_image, filters, mask):
             # Get the masks Region of Interest
             roi_mask = mask[y:y + roi_size[0], x:x + roi_size[1]]
 
+            band_min = np.min(roi_mask)
+
             # Check if this region is completely inside a band
-            if np.min(roi_mask) == np.max(roi_mask):
+            if band_min == np.max(roi_mask) and band_min != 0:  # Exclude the color 0, which is regions we do not want
                 # calculate/Count up the total of each value in image as a histogram of directions in the image
                 roi = pixel_directions[y:y + roi_size[0], x:x + roi_size[1]]
                 unique, unique_counts = np.unique(roi, return_counts=True)
@@ -95,7 +126,16 @@ def run_gabor(color_image, filters, mask):
                 bins = np.zeros((orientations,))
                 bins[unique] = unique_counts
 
+                # Generate color histogram
+                roi_color = color_image[y:y + roi_size[0], x:x + roi_size[1]]
+                color_hist = create_color_histogram(roi_color)
+                color_hist = np.array(color_hist).flatten()
+
+                # Get the band in a 1,2,3,4 etc fashion
+                band = np.where(band_map == band_min)[0][0]
+
                 # store those ROI Histograms in a file
+                save_hogs(bins, (y, x), band, output_file, color_hist=color_hist)
 
                 # Display the Region during debug mode for examples
                 if (np.argmax(bins) > 0) and DEBUG is True:
@@ -104,7 +144,8 @@ def run_gabor(color_image, filters, mask):
             else:
                 print("Tossing region", y, x) if DEBUG else None
 
-
+    output_file.close()
+    return end_process - start_process
     # run the data from the files through ANN
 
 
@@ -161,7 +202,7 @@ def create_color_histogram(image, bins=8):
 
 # Save the hog information for a single HOG/Region
 # Along with the coordinates, and the band prediction.
-def save_hogs(hog_info, region_coords, band, output_file):
+def save_hogs(hog_info, region_coords, band, output_file, color_hist=None):
     """
     Saves the HOG generated from the single ROI to the file
 
@@ -178,6 +219,12 @@ def save_hogs(hog_info, region_coords, band, output_file):
     output_file.write(',')
     output_file.write(str(csv_hog_info))
     output_file.write(',')
+
+    if color_hist is not None:
+        csv_color_hist = ','.join([('%f' % num).rstrip('0').rstrip('.') for num in color_hist])
+        output_file.write(str(csv_color_hist))
+        output_file.write(',')
+
     output_file.write(str(csv_region_coords))
     output_file.write('\n')
 
@@ -201,25 +248,63 @@ def load_hogs_csv(directory):
 
     return all_hogs
 
+
+def resize_image_to_mask(image, mask):
+    r = mask.shape[1] / image.shape[1]
+    dim = (mask.shape[1], int(image.shape[0] * r))
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+
+    return resized
+
+
+def run_gabor_on_directory(directory, mask):
+    start_time = time.time()
+
+    orientations = 16
+    filters = build_filters(orientations)
+
+    for filename in os.listdir(directory):
+        print("Start: ", filename)
+        combined_filename = os.path.join(directory, filename)
+        image_color = cv2.imread(combined_filename)
+
+        # Make sure the image was loaded, if not, probably an invalid file format (text file?)
+        if image_color is None:
+            print('Invalid file:', filename)
+            continue
+
+        # Make sure the images are the correct dimensions, if not, resize to mask size
+        if mask.shape[0:2] != image_color.shape[0:2]:
+            print('Image size mismatch, resizing:', filename)
+            image_color = resize_image_to_mask(image_color, mask)
+
+        # Generate and save ALL hogs for this image
+        run_gabor(image_color, filters, mask, combined_filename, orientations)
+
+    print("Total Runtime:", time.time() - start_time)
+
+
 if __name__ == '__main__':
     mask_filename = r"../mask_generator/gray-mask.png"
 
     # real mask
-    mask = cv2.imread(mask_filename)
+    mask = cv2.imread(mask_filename, cv2.IMREAD_GRAYSCALE)
 
-    print(__doc__)
-    try:
-        img_fn = sys.argv[1]
-    except:
-        img_fn = './baboon.jpg'
-
-    img = cv2.imread(img_fn)
-    if img is None:
-        print('Failed to load image file:', img_fn)
-        sys.exit(1)
-
-    filters = build_filters(16)
-
-    res1_start = time.time()
-    res1 = run_gabor(img, filters, mask)
-    print("Single Threaded:", time.time() - res1_start)
+    run_gabor_on_directory('../image_subtractor/images/', mask)
+    #
+    # try:
+    #     image_filename = sys.argv[1]
+    # except:
+    #     image_filename = './baboon.jpg'
+    #
+    # img = cv2.imread(image_filename)
+    # if img is None:
+    #     print('Failed to load image file:', image_filename)
+    #     sys.exit(1)
+    #
+    # orientations = 16
+    # filters = build_filters(orientations)
+    # res1_start = time.time()
+    # timeframe = run_gabor(img, filters, mask, image_filename, orientations)
+    # print("Total Runtime:", time.time() - res1_start)
+    # print("Gabor Runtime:", timeframe)
